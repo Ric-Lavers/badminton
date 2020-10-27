971557f09cae69127b5e53e078c791fc22a5808f
Ric Lavers <rlavers@deloitte.com.au>
2020-10-27 16:35:16 +1100

    services

--- rsvp.js
+++ rsvp.js
@@ -1,33 +1,84 @@
-(()=>{
+anotherMobileBtn = document.querySelector("#add-mobile")
+anotherGuestBtn = document.querySelector("#add-guest")
+;(() => {
+  anotherMobileBtn.addEventListener("click", anotherMobile)
+  anotherGuestBtn.addEventListener("click", anotherName)
 
-  form=document.querySelector('#rsvp-form')
-  form.addEventListener('submit', e => {
-    e.preventDefault()
-    data=Array.from(
-      form.querySelectorAll('input:not([type="submit"])')
-    ).reduce((a,c) => {
-      a[c.name]= c.value
-      return a
-    },{})
-    console.log(data);
-
-})
-
+  form = document.querySelector("#rsvp-form")
+  form.addEventListener("submit", submitForm)
 })()
 
-function submitForm(e){
+async function submitForm(e) {
   e.preventDefault()
   //* format data
-  data=Array.from(
-    form.querySelectorAll('input:not([type="submit"])')
-  ).reduce((a,c) => {
-    a[c.name]= c.value
-    return a
-  },{})
-  console.log(data);
+  data = Array.from(form.querySelectorAll('input:not([type="submit"])')).reduce(
+    (a, c) => {
+      if (c.name.includes("guest")) {
+        a.guests.push(c.value)
+        return a
+      }
+      if (c.name.includes("number")) {
+        a.numbers.push(c.value)
+        return a
+      }
+      a[c.name] = c.value
+
+      return a
+    },
+    { guests: [], numbers: [] },
+  )
 
   //* send data
-  
-  
-  //* confirm send (set localStorage)
+  try {
+    if (!localStorage._id) {
+      const team = await createTeam(data)
+      localStorage.setItem("_id", team._id)
+      // hide form / show edit button
+    } else {
+      const team = await updateTeam({ ...data, _id: localStorage._id })
+    }
+    const teams = await getAllTeams()
+
+    // populate bracket
+  } catch (error) {}
 }
+
+function anotherMobile(e) {
+  e.preventDefault()
+  input = document.createElement("input")
+  input.setAttribute("name", "number_2")
+  input.setAttribute("type", "text")
+  anotherMobileBtn.replaceWith(input)
+}
+function anotherName(e) {
+  e.preventDefault()
+  input = document.createElement("input")
+  input.setAttribute("name", "guest_2")
+  input.setAttribute("type", "text")
+  anotherGuestBtn.replaceWith(input)
+}
+
+function showRSVP() {
+  editRsvp = document.getElementById("edit-rsvp")
+  rsvpForm = document.getElementById("rsvp-form-container")
+
+  rsvpForm.removeAttribute("hidden", "false")
+  editRsvp.setAttribute("hidden", "true")
+}
+function hideRSVP() {
+  editRsvp = document.getElementById("edit-rsvp")
+  rsvpForm = document.getElementById("rsvp-form-container")
+
+  rsvpForm.setAttribute("hidden", "true")
+  editRsvp.removeAttribute("hidden")
+}
+
+function setRsvpView() {
+  showRsvpBtn = document.getElementById("show-rsvp")
+  showRsvpBtn.addEventListener("click", showRSVP)
+  if (localStorage._id) {
+    hideRSVP()
+  }
+}
+
+setRsvpView()
---
+++ services.js
@@ -0,0 +1,30 @@
+var base =
+  "https://skribbl-lists-serverless.now.sh" ||
+  "https://skribbl-lists-serverless.ric-lavers.vercel.app" ||
+  "http://localhost:3000"
+
+const getAllTeams = async () => {
+  try {
+    const teams = await fetch(base + "/api/team/all")
+    return teams.json()
+  } catch (error) {}
+}
+const createTeam = async (data) => {
+  try {
+    const team = await fetch(base + "/api/team/create", {
+      method: "POST",
+      body: JSON.stringify(data),
+    })
+
+    return team.json()
+  } catch (error) {}
+}
+const updateTeam = async (data) => {
+  try {
+    const team = await fetch(base + "/api/team/update", {
+      method: "POST",
+      body: JSON.stringify(data),
+    })
+    return team.json()
+  } catch (error) {}
+}
